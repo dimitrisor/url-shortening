afb0f156af0b0b7584a464ad7a0595653f77bf92
Dimitris <dimitrisorfanudakis@gmail.com>
2021-02-15 03:58:51 +0200

    Bug fix
    Fix incorrect ProviderException, super call argument

--- shorty/exception/provider_exception.py
+++ shorty/exception/provider_exception.py
@@ -3,4 +3,4 @@ from shorty.exception.system_exception import SystemException
 SERVICE_UNAVAILABLE_MESSAGE = "Something went wrong on our side, please try again later"
 class ProviderException(SystemException):
     def __init__(self, code: str):
-        super().__init__(HTTPStatus.SERVICE_UNAVAILABLE, code, self.SERVICE_UNAVAILABLE_MESSAGE)
+        super().__init__(HTTPStatus.SERVICE_UNAVAILABLE, code, SERVICE_UNAVAILABLE_MESSAGE)
