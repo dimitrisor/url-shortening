c701492756c1605e737c17238a292b713304150e
Dimitris <dimitrisorfanudakis@gmail.com>
2021-02-15 21:01:28 +0200

    Bug fix ShortyService
    Fix the wrong behavior of the provider chain.
    Provider chain now handles >2 unlimited number of providers

--- shorty/service/shorty_service.py
+++ shorty/service/shorty_service.py
@@ -1,19 +1,21 @@
+from shorty.service.provider.bitly_clone_provider import BitlyCloneProvider
 from shorty.service.provider.provider import Provider
 from shorty.service.provider.bitly_provider import BitlyProvider
 from shorty.service.provider.tinyurl_provider import TinyUrlProvider
 
 class ShortyService:
     default_provider_name = 'tinyurl'
-    providers = {"tinyurl":TinyUrlProvider, "bitly":BitlyProvider}
+    providers = { "tinyurl":TinyUrlProvider, "bitly":BitlyProvider, "bitlyclone":BitlyCloneProvider}
 
     @classmethod
     def get_provider_chain(cls, selected_name: str) -> Provider:
-        selected_provider = cls.providers.get(selected_name)()
+        provider = cls.providers.get(selected_name)()
+        provider_chain = provider
         for provider_name in cls.providers.keys():
             if provider_name != selected_name:
-                next_provider = cls.providers.get(provider_name)
-                selected_provider.set_next(next_provider())
-        return selected_provider
+                next_provider_cls = cls.providers.get(provider_name)
+                provider = provider.set_next(next_provider_cls())
+        return provider_chain
 
     @classmethod
     def get_provider_names(cls) -> list:
