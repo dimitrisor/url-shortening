e5e2b8d788f7fb90b47b3344ab31215e55cdc3c1
Dimitris <dimitrisorfanudakis@gmail.com>
2021-02-25 13:06:55 +0200

    Test: Adds test for 404 not found requested url

--- tests/test_api.py
+++ tests/test_api.py
@@ -32,3 +32,17 @@ def test_api_when_request_is_invalid(post, cls, provider, expected_status, expec
     response_data = response.get_json()
     assert response.status_code == int(expected_status)
     assert response_data == {"message":expected_message}
+
+def test_api_when_request_url_is_invalid_not_found(post):
+    # GIVEN
+    cls = "http://www.example.com"
+    provider = "bitly"
+    invalid_endpoint = '/non_existent'
+    # WHEN
+    request_data = {"cls": cls, "provider": provider}
+    response = post(invalid_endpoint, data=request_data)
+
+    # THEN
+    response_data = response.get_json()
+    assert response.status_code == 404
+    assert response_data == {"message": "The requested URL was not found on the server"}
