e0be50a54497584d972d3290a489d55d7ffeb4d0
Dimitris <dimitrisorfanudakis@gmail.com>
2021-03-30 12:01:05 +0300

    Update api
    - Update test_api.py. Mock provider.post_data method to avoid calling requests.post
    - Minor updates in the api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,21 +1,32 @@
+from unittest.mock import Mock
 import pytest
 from http import HTTPStatus
+from shorty.service.provider.bitly_provider import BitlyProvider
 from shorty.service.shorty_service import ShortyService
+
+def mock_bitly_provider(expected_link):
+    # Mock post_data, avoid calling the real requests.post
+    mock_bitly_post_resp = Mock()
+    mock_bitly_post_resp.status_code = 200
+    mock_bitly_post_resp.json = Mock(return_value = {'link': expected_link})
+    BitlyProvider.post_data = Mock(return_value = mock_bitly_post_resp)
 
 def test_api_when_request_is_valid(post):
     # GIVEN
     url = "http://www.example.com"
     provider = "bitly"
     endpoint = '/shortlinks'
+
+    expected_link = 'https://bit.ly/example'
+    mock_bitly_provider(expected_link)
     # WHEN
-    request_data = {"url": url, "provider": provider}
-    response = post(endpoint, data=request_data)
+    response = post(endpoint, data={"url": url, "provider": provider})
 
     # THEN
     response_data = response.get_json()
     assert response.status_code == HTTPStatus.OK.real
     assert response_data['url'] == url
-    assert response_data['link'] is not None
+    assert response_data['link'] == expected_link
 
 @pytest.mark.parametrize(
     "post, url, provider, expected_status, expected_message",[
@@ -37,7 +48,7 @@ def test_api_when_request_payload_is_invalid(post, url, provider, expected_statu
     assert response.status_code == int(expected_status)
     assert response_data == {"message":expected_message}
 
-def test_api_when_request_url_is_invalid_not_found(post):
+def test_api_when_request_url_is_not_found(post):
     # GIVEN
     url = "http://www.example.com"
     provider = "bitly"
